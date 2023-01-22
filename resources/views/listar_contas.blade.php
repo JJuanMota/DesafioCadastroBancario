f4dd93a39102fbc82b50ee347d5351ad43dac688
juanm <juanmotagt@gmail.com>
2023-01-22 13:15:56 -0300

    init commit

---
+++ resources/views/listar_contas.blade.php
@@ -0,0 +1,21 @@
+@extends('components.app')
+@section('content')
+<br><br>
+<table>
+    <tr>
+        <th>Nome: </th>
+        <th>CPF: </th>
+        <th>Número da conta: </th>
+        <th>Editar: </th>
+        <th>Remover: </th>
+
+    </tr>
+    @foreach($contas as $conta)
+        <tr>
+            <td>{{$conta['pessoa']['nome']}}</td>
+            <td>{{$conta['pessoa']['cpf']}}</td>
+            <td>{{$conta['conta']['numero_conta']}}</td>
+        </tr>
+    @endforeach
+</table>
+@endsection
---
+++ resources/views/listar_movimentacao.blade.php
@@ -0,0 +1,24 @@
+@extends('components.app')
+@section('content')
+<br><br>
+<table>
+    <tr>
+        <th>Nome: </th>
+        <th>CPF: </th>
+        <th>Número da conta: </th>
+        <th>Valor: </th>
+        <th>Deposito/Retirada: </th>
+        <th>Editar: </th>
+        <th>Remover: </th>
+    </tr>
+    @foreach($movimentacoes as $movimentacao)
+        <tr>
+            <td>{{$movimentacao['pessoa']['nome']}}</td>
+            <td>{{$movimentacao['pessoa']['cpf']}}</td>
+            <td>{{$movimentacao['conta']['numero_conta']}}</td>
+            <td>{{$movimentacao['movimentacao']['valor']}}</td>
+            <td>{{$movimentacao['movimentacao']['dep_ret'] == "ret" ? "Retirada" : "Deposito"}}</td>
+        </tr>
+    @endforeach
+</table>
+@endsection
